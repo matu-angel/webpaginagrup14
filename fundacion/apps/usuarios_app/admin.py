512c2ed194d4692b86c7ab7dd29f5973c87c93be
matu-angel <mattiasvelazquez@gmail.com>
2022-09-02 12:30:14 -0300

    visualizacion

--- fundacion/apps/usuarios_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.admin.decorators import register
-from .models import Profile
-
-# Register your models here.
-@admin.register(Profile)
-class ProfileUsers(admin.ModelAdmin):
-    list_display= ('usuario', 'resume', 'usr_twitter')
